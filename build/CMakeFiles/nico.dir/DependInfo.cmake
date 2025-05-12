d302167a791efe34151a63ede47c98b8376f85ae
= <=>
2025-05-12 10:13:41 -0500

    adding build directory

---
+++ build/CMakeFiles/nico.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/archerg/Desktop/projects/nicotine/src/main.cpp" "CMakeFiles/nico.dir/src/main.cpp.o" "gcc" "CMakeFiles/nico.dir/src/main.cpp.o.d"
+  "/Users/archerg/Desktop/projects/nicotine/src/parseTree.cpp" "CMakeFiles/nico.dir/src/parseTree.cpp.o" "gcc" "CMakeFiles/nico.dir/src/parseTree.cpp.o.d"
+  "/Users/archerg/Desktop/projects/nicotine/src/tokenize.cpp" "CMakeFiles/nico.dir/src/tokenize.cpp.o" "gcc" "CMakeFiles/nico.dir/src/tokenize.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
